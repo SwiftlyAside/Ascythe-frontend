208a52d03f4e83b699f598b9bfe9d977e8bb144c
Swiftly <ivecis@live.co.kr>
2020-10-12 12:33:13 +0900

    Implement of AuthService, PostService
    Preflight 데이터 처리, JWT인증, 글목록 표시 구현

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {LoginRequestPayload} from './login.request.payload';
 import {AuthService} from '../shared/auth.service';
+import {ActivatedRoute, Router} from '@angular/router';
+import {ToastrService} from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -11,8 +13,11 @@ import {AuthService} from '../shared/auth.service';
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
   loginRequestPayload: LoginRequestPayload;
+  registerSuccessMessage: string;
+  isError: boolean;
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private activatedRoute: ActivatedRoute,
+              private router: Router, private toastr: ToastrService) {
     this.loginRequestPayload = {
       username: '',
       password: ''
@@ -24,14 +29,29 @@ export class LoginComponent implements OnInit {
       username: new FormControl('', Validators.required),
       password: new FormControl('', Validators.required)
     });
+
+    this.activatedRoute.queryParams
+      .subscribe(params => {
+        if (params.registered !== undefined && params.registered === 'true') {
+          this.toastr.success('회원가입 완료');
+          this.registerSuccessMessage = '이메일을 확인해주세요 '
+            + '로그인 하기 전에 계정을 활성화해주세요!';
+        }
+      });
   }
 
   login(): void {
     this.loginRequestPayload.username = this.loginForm.get('username').value;
     this.loginRequestPayload.password = this.loginForm.get('password').value;
 
-    this.authService.login(this.loginRequestPayload).subscribe(value => {
-      console.log('Login Successful');
+    this.authService.login(this.loginRequestPayload).subscribe(data => {
+      if (data) {
+        this.isError = false;
+        this.router.navigateByUrl('');
+        this.toastr.success('로그인 했어요.');
+      } else {
+        this.isError = true;
+      }
     });
   }
 }
--- src/app/auth/shared/auth.service.ts
+++ src/app/auth/shared/auth.service.ts
@@ -1,16 +1,20 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {SignupRequestPayload} from '../signup/signup-request.payload';
-import {Observable} from 'rxjs';
+import {Observable, throwError} from 'rxjs';
 import {LocalStorageService} from 'ngx-webstorage';
 import {LoginRequestPayload} from '../login/login.request.payload';
 import {LoginResponse} from '../login/login-response.payload';
-import {map} from 'rxjs/operators';
+import {map, tap} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  refreshTokenPayload = {
+    refreshToken: this.getRefreshToken(),
+    username: this.getUserName()
+  };
 
   constructor(private httpClient: HttpClient, private localStorage: LocalStorageService) {
   }
@@ -28,5 +32,48 @@ export class AuthService {
         this.localStorage.store('expiresAt', data.expiresAt);
         return true;
       }));
+  }
+
+  getJwtToken(): any {
+    return this.localStorage.retrieve('authenticationToken');
+  }
+
+  refreshToken(): Observable<LoginResponse> {
+    return this.httpClient.post<LoginResponse>('http://localhost:8080/api/auth/refresh/token',
+      this.refreshTokenPayload)
+      .pipe(tap(response => {
+        this.localStorage.clear('authenticationToken');
+        this.localStorage.clear('expiresAt');
+
+        this.localStorage.store('authenticationToken',
+          response.authenticationToken);
+        this.localStorage.store('expiresAt', response.expiresAt);
+      }));
+  }
+
+  logout(): void {
+    this.httpClient.post('http://localhost:8080/api/auth/logout', this.refreshTokenPayload,
+      {responseType: 'text'})
+      .subscribe(data => {
+        console.log(data);
+      }, error => {
+        throwError(error);
+      });
+    this.localStorage.clear('authenticationToken');
+    this.localStorage.clear('username');
+    this.localStorage.clear('refreshToken');
+    this.localStorage.clear('expiresAt');
+  }
+
+  getUserName(): any {
+    return this.localStorage.retrieve('username');
+  }
+
+  getRefreshToken(): any {
+    return this.localStorage.retrieve('refreshToken');
+  }
+
+  isLoggedIn(): boolean {
+    return this.getJwtToken() != null;
   }
 }
