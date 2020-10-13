c290da9196234ac7e77f583d5418a7488b237587
Swiftly <ivecis@live.co.kr>
2020-10-13 18:06:12 +0900

    Add some components
    화면 구성요소 추가

---
+++ src/app/shared/topic-side-bar/topic-side-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-topic-side-bar',
+  templateUrl: './topic-side-bar.component.html',
+  styleUrls: ['./topic-side-bar.component.css']
+})
+export class TopicSideBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
